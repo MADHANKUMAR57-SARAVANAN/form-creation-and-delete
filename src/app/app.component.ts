0e029e7f35d3d8b52b28ef22bb942470ebba786e
unknown <madhankumar.vs@gmail.com>
2020-06-16 21:47:56 +0530

    new project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TabledataDelete';
+  term: string;
+  public id : number;
+  public name : string;
+  public age : number;
+  public gender : string;
+  public country : string;
+
+  public rows : Array<{id : number,name : string,age : number,gender : string,country : string}> =[];
+
+  headers = ["ID","Name","Age","Gender","Country","Action"];
+
+  // rows =[
+  //   {
+  //     "ID":"1",
+  //     "Name":"Madhan",
+  //     "Age":"25",
+  //     "Gender":"Male",
+  //     "Country":"Indian",
+  //   },
+  //   {
+  //     "ID":"2",
+  //     "Name":"Azhar",
+  //     "Age":"24",
+  //     "Gender":"Male",
+  //     "Country":"Indian",
+
+  //   }
+  // ]
+
+  deleteEmployee(i){
+    console.log(i)
+    const index = this.rows.indexOf(i);
+    console.log(index);
+    this.rows.splice(index,5);
+    console.log(index)
+  }
+
+  buttonClicked(){
+    this.rows.push({id : this.id , name:this.name , age : this.age , gender : this.gender , country : this.country});
+    this.id = null;
+    this.name = null;
+    this.age = null;
+    this.gender = null;
+    this.country = null;
+  }
 }
